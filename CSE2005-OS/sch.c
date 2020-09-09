047c1b024df96445e2b28bb80acb39b43c601334
AbhinavGor <abhinav20016@gmail.com>
2020-09-09 18:06:43 +0530

    Added code SJF and Priority Scheduling

--- CSE2005-OS/sch.c
+++ CSE2005-OS/sch.c
@@ -5,7 +5,7 @@
 typedef struct{
 int pid;
 int arrival_time; int burst_time; int waiting_time;
-int turnaround_time; int completion_time;
+int turnaround_time; int completion_time; int priority;
 }Process;
 
 void print_table(Process p[], int n){
@@ -31,9 +31,14 @@ void getProcesses(Process pr[], int num){
 
         printf("\nEnter the Arrival Time:\n");
         scanf("%d", &pr[i].arrival_time);    
+
+        printf("\nEnter the Priority:\n");
+        scanf("%d", &pr[i].priority);    
     }
 }
 
+
+//FCFS
 void waitFCFS(Process pr[], int n){
     pr[0].waiting_time = 0;
     for(int i = 1; i < n; i++){
@@ -59,6 +64,7 @@ void FCFS(Process pr[], int num, int time){
     print_table(pr, num);
 }
 
+//SJF
 void sortSJF(Process pr[], int n){
     Process temp;
     for(int i =0 ; i < n - 1; i++){
@@ -73,15 +79,77 @@ void sortSJF(Process pr[], int n){
 }
 
 void waitSJF(Process pr[], int num){
-    for(int i = 0; i < num; i++){
+    for(int j = 0; j < num; j++){
+        pr[i].waiting_time = 0;
+        for(int i = 0; i < j; i++){
+            pr[i].waiting_time = pr[j].burst_time + pr[j].waiting_time;
+        }
+    }
+}
 
+void tatSJF(Process pr[], int n){
+    for(int i = 0; i < n; i++){
+        pr[i].turnaround_time = pr[i].burst_time + pr[i].waiting_time;
     }
 }
 
 void SJF(Process pr[], int num){
     sortSJF(pr, num);
     waitSJF(pr, num);
+    tatSJF(pr, num);
+    print_table(pr, n);
 }
+
+// //SRTF
+// void SRTF(Process pr[], int n, int time){
+//     sortSJF(pr, n);
+//     waitSRTF(pr, n);
+//     tatSRTF(pr, n);
+// }
+
+// void waitSRTF(Process pr[], int n){
+
+// }
+
+// void tatSRTF(Process pr[], int n){
+
+// }
+
+//
+
+//Priority
+void Priority(Process, pr[], int n, int time){
+    sortPrior(pr, n);
+    waitSJF(pr, n);
+    tatSJF(pr, n);
+    print_table(pr, n);
+}
+
+void sortPrior(Process pr[], int n){
+    for(int i = 0; i < n; i++){
+        int key = pr[i].priority;
+        int j = i - 1;
+
+        while (j >= 0 && pr[j].priority > key)
+        {
+            pr[j + 1].priority = pr[j].priority;
+            j -= 1;
+        }
+
+        pr[j + 1].priority = key;
+        
+    }
+}
+
+void waitPrior(Process pr[], int n){
+
+}
+
+//RoundRobin
+void roundRobin(Process pr[], int n, int time){
+
+}
+
 float avgTAT(Process pr[], int n){
     int avg = 0;
     for(int i = 0; i < n; i++){
